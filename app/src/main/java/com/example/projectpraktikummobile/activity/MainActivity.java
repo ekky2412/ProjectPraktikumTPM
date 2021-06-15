cc6d4b9b295271fee487fc86a9cef7c6fb580069
ekky2412 <ekky.raharjo@gmail.com>
2021-06-15 20:39:40 +0700

    add splash,update about

--- app/src/main/java/com/example/projectpraktikummobile/activity/MainActivity.java
+++ app/src/main/java/com/example/projectpraktikummobile/activity/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.projectpraktikummobile.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.projectpraktikummobile.R;
 
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int SPLASH_TIME_OUT = 3000;
+        new Handler().postDelayed(() -> {
+            Intent homeIntent = new Intent(MainActivity.this, MenuActivity.class);
+            startActivity(homeIntent);
+            finish();
+        }, SPLASH_TIME_OUT);
     }
 }
